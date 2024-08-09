18197e71f584017c8ae12b9054e2246878a00eb5
Victor Savonchik <savonvictor@gmail.com>
2024-08-09 15:42:27 +0300

    Lesson_11_2

---
+++ src/Triangle.java
@@ -0,0 +1,36 @@
+public class Triangle implements Shape {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private String fillColor;
+    private String borderColor;
+
+    public Triangle (double sideA, double sideB, double sideC, String fillColor, String borderColor) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return sideA + sideC + sideB;
+    }
+
+    @Override
+    public double getArea() {
+        double s = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
+    @Override
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    @Override
+    public String getBorderColor() {
+        return borderColor;
+    }
+}
