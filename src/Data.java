d87dbe9ccbe02a062a0f65a95c57c9e95aeec419
savon01 <130082350+savon01@users.noreply.github.com>
2024-08-25 16:17:32 +0300

    Delete src directory
    Delete src directory

--- src/Data.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Data {
-    public String getUrl = "https://www.mts.by/";
-    public String phone = "297777777";
-    public String amount = "2.0";
-    public String email = "example@example.com";
-}
